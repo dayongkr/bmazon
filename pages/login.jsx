9d6c1cad3836ca603b7ef3c73b53f5aa4a7fa3f3
dayongbz <dayongbz@gmail.com>
2019-10-28 16:20:05 +0900

    Update login.jsx

---
+++ pages/login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LoginWrapper = styled.div`
+  padding: 50px 30px;
+  background-color: white;
+`;
+
+const Login = () => {
+  return (
+    <LoginWrapper>
+      <h1>로그인</h1>
+    </LoginWrapper>
+  );
+};
+
+export default Login;
