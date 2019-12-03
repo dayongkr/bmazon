4d7e80c835a9ca2ac1a8adfa56c540f021952456
dayongbz <dayongbz@gmail.com>
2019-12-03 10:44:26 +0900

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -95,16 +95,15 @@ App.getInitialProps = async context => {
 const configureStore = (initialState, options) => {
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = [sagaMiddleware];
-  const enhancer =
-    process.env.NODE_ENV === 'production'
-      ? compose(applyMiddleware(...middlewares))
-      : compose(
-          applyMiddleware(...middlewares),
-          !options.isServer &&
-            typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
-            ? window.__REDUX_DEVTOOLS_EXTENSION__()
-            : f => f,
-        );
+  const enhancer = process.env.NODE_ENV === 'production';
+  // ? compose(applyMiddleware(...middlewares)):
+  compose(
+    applyMiddleware(...middlewares),
+    !options.isServer &&
+      typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f,
+  );
   const store = createStore(reducer, initialState, enhancer);
   store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
